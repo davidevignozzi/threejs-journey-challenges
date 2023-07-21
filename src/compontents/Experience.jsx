a39b20e979a2ef6faef98899481aaa252bacab15
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-21 15:41:34 +0200

    :sparkles: Stage

--- src/compontents/Experience.jsx
+++ src/compontents/Experience.jsx
@@ -1,32 +1,41 @@
-import { Center, Environment, OrbitControls } from '@react-three/drei';
+import { Center, OrbitControls, Stage } from '@react-three/drei';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import Gameboy from './Gameboy';
-import { useFrame, useThree } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
 import { useControls } from 'leva';
 
 const Experience = () => {
-  const debugCamera = useControls('Camera Position', {
-    x: 5,
-    y: 5,
-    z: 5
-  });
+  /**
+   * Debug Camera Position
+   */
+  // const debugCamera = useControls('Camera Position', {
+  //   x: 5,
+  //   y: 5,
+  //   z: 5
+  // });
 
-  useFrame((state) => {
-    state.camera.position.set(debugCamera.x, debugCamera.y, debugCamera.z);
-  });
+  // useFrame((state) => {
+  //   state.camera.position.set(debugCamera.x, debugCamera.y, debugCamera.z);
+  // });
 
   return (
     <>
       <OrbitControls />
 
       <EffectComposer>
-        <Bloom mipmapBlur />
+        <Bloom mipmapBlur luminanceThreshold={1.25} />
       </EffectComposer>
 
-      <Environment preset="apartment" />
-
       <Center>
-        <Gameboy />
+        <Stage
+          shadows="contact"
+          adjustCamera={5}
+          environment="apartment"
+          preset="soft"
+          intensity={0.2}
+        >
+          <Gameboy />
+        </Stage>
       </Center>
     </>
   );
