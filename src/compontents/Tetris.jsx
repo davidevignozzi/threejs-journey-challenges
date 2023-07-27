975e4f7c687ddf15bf715c2dc6eb8c2a43020d59
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-27 12:20:05 +0200

    :sparkles: Add randomness to tetris

--- src/compontents/Tetris.jsx
+++ src/compontents/Tetris.jsx
@@ -4,76 +4,76 @@ import { useMemo } from 'react';
 const scale = 0.5;
 const positionY = 4;
 
-const Block = ({ positionX = 0, positionY = 0 }) => {
+const Block = ({ positionX = 0, positionY = 0, color }) => {
   return (
     <mesh position-x={positionX} position-y={positionY} scale={scale}>
       <boxGeometry />
-      <meshStandardMaterial />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
 
-const IBlock = ({ startPositionY = 0 }) => {
+const IBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position-y={positionY + startPositionY}>
-      <Block positionY={scale * 2} />
-      <Block positionY={scale} />
-      <Block />
-      <Block positionY={-scale} />
+    <RigidBody position={[x, positionY + y, z]}>
+      <Block positionY={scale * 2} color={'red'} />
+      <Block positionY={scale} color={'red'} />
+      <Block color={'red'} />
+      <Block positionY={-scale} color={'red'} />
     </RigidBody>
   );
 };
 
-const TBlock = ({ startPositionY = 0 }) => {
+const TBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position-y={positionY + startPositionY}>
-      <Block positionX={scale} />
-      <Block positionX={-scale} />
-      <Block />
-      <Block positionY={-scale} />
+    <RigidBody position={[x, positionY + y, z]}>
+      <Block positionX={scale} color={'cyan'} />
+      <Block positionX={-scale} color={'cyan'} />
+      <Block color={'cyan'} />
+      <Block positionY={-scale} color={'cyan'} />
     </RigidBody>
   );
 };
 
-const JBlock = ({ startPositionY = 0 }) => {
+const JBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position-y={positionY + startPositionY}>
-      <Block positionY={scale} />
-      <Block />
-      <Block positionX={scale} />
-      <Block positionX={scale * 2} />
+    <RigidBody position={[x, positionY + y, z]}>
+      <Block positionY={scale} color={'orange'} />
+      <Block color={'orange'} />
+      <Block positionX={scale} color={'orange'} />
+      <Block positionX={scale * 2} color={'orange'} />
     </RigidBody>
   );
 };
 
-const OBlock = ({ startPositionY = 0 }) => {
+const OBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position-x={-0.25} position-y={positionY + startPositionY}>
-      <Block />
-      <Block positionX={scale} />
-      <Block positionX={scale} positionY={scale} />
-      <Block positionY={scale} />
+    <RigidBody position={[-0.25 + x, positionY + y, z]}>
+      <Block color={'mediumpurple'} />
+      <Block positionX={scale} color={'mediumpurple'} />
+      <Block positionX={scale} positionY={scale} color={'mediumpurple'} />
+      <Block positionY={scale} color={'mediumpurple'} />
     </RigidBody>
   );
 };
 
-const SBlock = ({ startPositionY = 0 }) => {
+const SBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position-y={positionY + startPositionY}>
-      <Block positionX={-scale} positionY={scale} />
-      <Block positionY={scale} />
-      <Block />
-      <Block positionX={scale} />
+    <RigidBody position={[x, positionY + y, z]}>
+      <Block positionX={-scale} positionY={scale} color={'greenyellow'} />
+      <Block positionY={scale} color={'greenyellow'} />
+      <Block color={'greenyellow'} />
+      <Block positionX={scale} color={'greenyellow'} />
     </RigidBody>
   );
 };
 
 const Tetris = ({
-  count = 10,
+  count = 25,
   types = [IBlock, TBlock, JBlock, OBlock, SBlock]
 }) => {
   /**
-   *
+   * Generate an array of random blocks
    */
   const blocks = useMemo(() => {
     const blocks = [];
@@ -88,15 +88,14 @@ const Tetris = ({
 
   return (
     <group>
-      {/* <IBlock />
-      <TBlock />
-      <JBlock />
-      <OBlock />
-      <SBlock /> */}
-
       {blocks.map((TetrisItem, index) => {
         return (
-          <TetrisItem key={index} startPositionY={index + Math.random()} />
+          <TetrisItem
+            key={index}
+            x={Math.random() * 10 - 5}
+            y={index}
+            z={Math.random() * 10 - 5}
+          />
         );
       })}
     </group>
