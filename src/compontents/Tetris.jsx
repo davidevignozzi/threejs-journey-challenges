4a32059f1d5d1957aa84aaafcd3dc117099f57af
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-07-27 12:29:27 +0200

    :technologist: Explained Tetris Component

--- src/compontents/Tetris.jsx
+++ src/compontents/Tetris.jsx
@@ -4,7 +4,20 @@ import { useMemo } from 'react';
 const scale = 0.5;
 const positionY = 4;
 
-const Block = ({ positionX = 0, positionY = 0, color }) => {
+/**
+ * Each Cube has
+ * positionX and positionY
+ * to create tetris shapes
+ */
+
+/**
+ * Then each block has
+ * x,y,z
+ * to position the tetris shape randomly in the scene
+ *
+ */
+
+const Cube = ({ positionX = 0, positionY = 0, color }) => {
   return (
     <mesh position-x={positionX} position-y={positionY} scale={scale}>
       <boxGeometry />
@@ -15,55 +28,55 @@ const Block = ({ positionX = 0, positionY = 0, color }) => {
 
 const IBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position={[x, positionY + y, z]}>
-      <Block positionY={scale * 2} color={'red'} />
-      <Block positionY={scale} color={'red'} />
-      <Block color={'red'} />
-      <Block positionY={-scale} color={'red'} />
+    <RigidBody position={[x, y, z]}>
+      <Cube positionY={scale * 2} color={'red'} />
+      <Cube positionY={scale} color={'red'} />
+      <Cube color={'red'} />
+      <Cube positionY={-scale} color={'red'} />
     </RigidBody>
   );
 };
 
 const TBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position={[x, positionY + y, z]}>
-      <Block positionX={scale} color={'cyan'} />
-      <Block positionX={-scale} color={'cyan'} />
-      <Block color={'cyan'} />
-      <Block positionY={-scale} color={'cyan'} />
+    <RigidBody position={[x, y, z]}>
+      <Cube positionX={scale} color={'cyan'} />
+      <Cube positionX={-scale} color={'cyan'} />
+      <Cube color={'cyan'} />
+      <Cube positionY={-scale} color={'cyan'} />
     </RigidBody>
   );
 };
 
 const JBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position={[x, positionY + y, z]}>
-      <Block positionY={scale} color={'orange'} />
-      <Block color={'orange'} />
-      <Block positionX={scale} color={'orange'} />
-      <Block positionX={scale * 2} color={'orange'} />
+    <RigidBody position={[x, y, z]}>
+      <Cube positionY={scale} color={'orange'} />
+      <Cube color={'orange'} />
+      <Cube positionX={scale} color={'orange'} />
+      <Cube positionX={scale * 2} color={'orange'} />
     </RigidBody>
   );
 };
 
 const OBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position={[-0.25 + x, positionY + y, z]}>
-      <Block color={'mediumpurple'} />
-      <Block positionX={scale} color={'mediumpurple'} />
-      <Block positionX={scale} positionY={scale} color={'mediumpurple'} />
-      <Block positionY={scale} color={'mediumpurple'} />
+    <RigidBody position={[-0.25 + x, y, z]}>
+      <Cube color={'mediumpurple'} />
+      <Cube positionX={scale} color={'mediumpurple'} />
+      <Cube positionX={scale} positionY={scale} color={'mediumpurple'} />
+      <Cube positionY={scale} color={'mediumpurple'} />
     </RigidBody>
   );
 };
 
 const SBlock = ({ x = 0, y = 0, z = 0 }) => {
   return (
-    <RigidBody position={[x, positionY + y, z]}>
-      <Block positionX={-scale} positionY={scale} color={'greenyellow'} />
-      <Block positionY={scale} color={'greenyellow'} />
-      <Block color={'greenyellow'} />
-      <Block positionX={scale} color={'greenyellow'} />
+    <RigidBody position={[x, y, z]}>
+      <Cube positionX={-scale} positionY={scale} color={'greenyellow'} />
+      <Cube positionY={scale} color={'greenyellow'} />
+      <Cube color={'greenyellow'} />
+      <Cube positionX={scale} color={'greenyellow'} />
     </RigidBody>
   );
 };
@@ -93,7 +106,7 @@ const Tetris = ({
           <TetrisItem
             key={index}
             x={Math.random() * 10 - 5}
-            y={index}
+            y={index + 4}
             z={Math.random() * 10 - 5}
           />
         );
